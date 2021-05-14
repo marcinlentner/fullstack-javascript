f68b5405a666b43a9e662e64e4d0ae79bae0c1f9
Marcin Lentner <marcin_len@yahoo.co.uk>
2021-05-14 07:32:51 +0100

    http modules as client

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// import default, { nonDefault } export from config
-import config, { nodeEnv, logStars } from './config';
+// https module can be used both as client and server
+import https from 'https';
 
-logStars('Function');
+https.get('https://www.example.com', res => {
+  console.log('Response status code: ', res.statusCode); // throws an error if address not found, website down
+
+  res.on('data', chunk => {
+    console.log(chunk.toString());
+  });
+});
